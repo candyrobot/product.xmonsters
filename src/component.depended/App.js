12de802bc6e9c3ebda799ccf9527ab9b45d91c43
Hideaki Tiánzhōng <rivalknockout@gmail.com>
2019-03-12 14:27:10 +0900

    create

---
+++ src/component.depended/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactList from 'react-list';
+import './App.css';
+import GridListWithTitlebars from './GridListWithTitlebars';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <GridListWithTitlebars />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/object/data.js
@@ -0,0 +1,22 @@
+export const photoSets = [
+  {
+    img: 'hoge',
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: 'hoge',
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: 'hoge',
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    img: 'hoge',
+    title: 'Image',
+    author: 'author',
+  },
+];
