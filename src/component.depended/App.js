0cd45df3be1cc88bb94b6b8a6e33670d561a90cc
佐藤浩斗 <hiro.cook.workout@gmail.com>
2019-04-25 14:49:21 +0900

    u-next追加

--- src/component.depended/App.js
+++ src/component.depended/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ReactList from 'react-list';
 import './App.css';
+import Header from './Header'
 import GridListWithTitlebars from './GridListWithTitlebars';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <GridListWithTitlebars />
       </div>
     );
