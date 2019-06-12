9f803695a91b7c7012e613eb118d3dcef367edfe
佐藤浩斗 <hiro.cook.workout@gmail.com>
2019-06-13 01:07:49 +0900

    Merge branch 'master' of https://github.com/candyrobot/product.xmonsters
    # Conflicts:
    #	src/component.depended/App.js

--- src/component.depended/AppContainer.js
+++ src/component.depended/AppContainer.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 import Item from './Item';
+import Helmet from 'react-helmet';
 
-
+/*
+-これをtileDateの中にどうにかしていれる
+-Qiitaの記事みたいに読み込まれたら自動でOGP化させるか
+   <Helmet
+    meta={[
+      {
+        { property: 'og:title', content: 'マニア必見!がっちび介の六尺集' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://gum.co/vhUXJ' },
+        { property: 'og:image', content: 'https://static-2.gumroad.com/res/gumroad/8634858819690/asset_previews/3e706f32f96b5e4907738736e5ed83b7/retina/No10.jpeg' },
+        { property: 'og:description', content: 'マニア必見!六尺集 #顔だし #大量ムラムラしたときにぜひ！😏👋まるでいっしょにいるかのような #水泳部 #水球 #ライフセーバー #GOGO のフォト集。' },
+      }
+    ]}
+   />
+*/
 const tileData = [
   {
     id: 1,
